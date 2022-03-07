d7a8ded8e35564a14fd1ef75573be35def63b48e
Aryansh23 <98678055+Aryansh23@users.noreply.github.com>
2022-03-07 23:10:19 +0530

    Update and rename ques3 (1).c to ques3.c

--- ques3.c
+++ ques3.c
@@ -4,15 +4,14 @@ int main()
   int n,a,b,c,d,e;
   printf("Enter the number:",n);
   scanf("%d",&n);
-
- 
-    a=n%10;
-    n=n/10;
-    if(c>=10)
-   { b=10;
-     c=n/b;
-     b=b*10;
-     }
+  a=n%10;
+  n=n/10;
+  if(c>=10)
+  {
+    b=10;
+    c=n/b;
+    b=b*10;
+  }
     d=n%b;
     n=n/b;
     e=n%10;
