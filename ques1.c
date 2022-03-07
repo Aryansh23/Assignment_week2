ab1835f4726cdb14894745abc24a245b2d916f92
Aryansh23 <98678055+Aryansh23@users.noreply.github.com>
2022-03-07 22:30:18 +0530

    Add files via upload

---
+++ ques1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+  int rev(char s1);
+  char s1[100],s2[100],temp;
+  printf("enter str1 ");
+  gets(s1);
+  printf("enter str2 ");
+  gets(s2);
+  int i,j,len;
+  for(i=0;s1[i]!='\0';i++);
+  for(j=0;s2[j]!='\0';j++,++i)
+  {
+  s1[i]=s2[j];}
+  s1[i]='\0';
+  
+  printf("concat str : %s",s1);
+  return 0;
+}
+int rev(char s1)
+{
+  int i,j,len;
+  char s1,temp
+  gets(s1);
+  len=strlen(s1);
+
+  for(i=0;i<len/2;i++)
+  
+  temp=s1[i];
+  s1[i]=s1[len-1-i];
+  s1[len-1-i]=temp;
+  printf("the reversed string is %s",s1);
+  
+}
